e7f5046986abcd2bdcab0229e0674b832438ccd9
paul.rivier <paul.r.ml@gmail.com>
2011-09-26 16:28:17 +0200

    forgot to include simple_record file

---
+++ lib/funkr/types/simple_record.rb
@@ -0,0 +1,32 @@
+module Funkr
+  module Types
+    
+    class SimpleRecord < Array
+      ### usage : r = SimpleRecord.new(Hash), then r.field
+      ### r = SimpleRecord.new( name: "Paul", age: 27 )
+      ### r.name => "Paul"  ; r.age => 27
+      ### name, age = r
+      
+      def initialize(key_vals)
+        @key_vals = key_vals
+        key_vals.each do |k,v|
+          getter = k.to_sym
+          setter = format("%s=", k.to_s).to_sym
+          define_singleton_method(getter){ @key_vals[k] }
+          define_singleton_method(setter){|nv| @key_vals[k] = nv}
+          self.push(v)
+        end
+      end
+      
+      def with(new_key_vals)
+        self.class.new(@key_vals.merge(new_key_vals))
+      end
+      
+      def to_hash; @key_vals; end
+      
+      def to_s; @key_vals.to_s; end
+      
+    end
+
+  end
+end
